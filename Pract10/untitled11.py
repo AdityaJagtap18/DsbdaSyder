facdbd7419babf9ff54c79c068d8b006780c6655
Aditya Jagtap <89527335+AdityaJagtap18@users.noreply.github.com>
2024-05-05 23:13:32 +0530

    Update untitled11.py

--- Pract10/untitled11.py
+++ Pract10/untitled11.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-csv_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-df = pd.read_csv(csv_url, header = None)
-col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Species']
-df = pd.read_csv(csv_url, names = col_names)
+
+df = pd.read_csv(Iris.csv)
 
 dfh = df.head()
 
@@ -26,10 +24,10 @@ import matplotlib.pyplot as plt
 
 fig, axes = plt.subplots(2, 2, figsize=(16, 8))
 axes[0,0].set_title("Distribution of First Column")
-axes[0,0].hist(df["Sepal_Length"]);
+axes[0,0].hist(df["SepalLengthCm"]);
 axes[0,1].set_title("Distribution of Second Column")
-axes[0,1].hist(df["Sepal_Width"]);
+axes[0,1].hist(df["SepalWidthCm"]);
 axes[1,0].set_title("Distribution of Third Column")
-axes[1,0].hist(df["Petal_Length"]);
+axes[1,0].hist(df["PetalLengthCm"]);
 axes[1,1].set_title("Distribution of Fourth Column")
-axes[1,1].hist(df["Petal_Width"]);
+axes[1,1].hist(df["PetalWidthCm"]);
